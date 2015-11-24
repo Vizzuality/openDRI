917090faf3643e0f855b90ef0f9bf8e66c938613
Adrián Pérez <adrian@simbiotica.es>
2015-11-24 22:53:57 +0100

    logo inside header for index

--- wp-content/themes/odri/header.php
+++ wp-content/themes/odri/header.php
@@ -46,11 +46,11 @@
 		<div id="container">
 
 			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
-<div id="map" style="height:400px"></div>
-				<div id="inner-header" class="wrap cf">
+<!-- <div id="map" style="height:400px"></div>
+ -->				<div id="inner-header" class="wrap cf">
 
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"> <img src="<?php echo home_url(); ?>/wp-content/themes/odri/logo-extended.svg" alt="<?php bloginfo('name'); ?>"> </a></p>
 
 					<?php // if you'd like to use the site description you can un-comment it below ?>
 					<?php // bloginfo('description'); ?>
