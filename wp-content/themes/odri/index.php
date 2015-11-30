0239659c86fc00193fb0430dae3d55a89fbb7aac
Adrián Pérez <adrian@simbiotica.es>
2015-11-30 10:54:21 +0100

    article header text on index MARKUP

--- wp-content/themes/odri/index.php
+++ wp-content/themes/odri/index.php
@@ -41,24 +41,19 @@
 					<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
 						<header class="article-header">
 							<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-							<p class="byline entry-meta vcard">
-	                            <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
-	   								/* the time the post was published */
-	   								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-	   								/* the author of the post */
-	   								'<span class="by">'.__( 'by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-								); ?>
-							</p>
 						</header>
 						<section class="entry-content cf">
 							<?php the_content(); ?>
 						</section>
 						<footer class="article-footer cf">
-							<p class="footer-comment-count">
-								<?php comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), __( '<span>%</span> Comments', 'bonestheme' ) );?>
+							<p class="byline entry-meta vcard">
+	                            <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
+	   								/* the author of the post */
+	   								'<span class="by">'.__( '', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>',
+	   								/* the time the post was published */
+	   								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>'
+								); ?>
 							</p>
-	     					<?php printf( '<p class="footer-category">' . __('filed under', 'bonestheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
-	     					 <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
 						</footer>
 					</article>
 					<? } ?>
@@ -100,23 +95,12 @@
 					<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
 						<header class="article-header">
 							<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-							<p class="byline entry-meta vcard">
-	                            <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
-	   								/* the time the post was published */
-	   								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-	   								/* the author of the post */
-	   								'<span class="by">'.__( 'by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-								); ?>
-							</p>
 						</header>
 						<section class="entry-content cf">
 							<?php the_content(); ?>
 						</section>
 						<footer class="article-footer cf">
-							<p class="footer-comment-count">
-								<?php comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), __( '<span>%</span> Comments', 'bonestheme' ) );?>
-							</p>
-	     					<?php printf( '<p class="footer-category">' . __('filed under', 'bonestheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
+	     					<?php printf( '<p class="footer-category">' . __('', 'bonestheme' ) . '%1$s</p>' , get_the_category_list(', ') ); ?>
 	     					 <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
 						</footer>
 					</article>
@@ -171,7 +155,7 @@
 							<p class="footer-comment-count">
 								<?php comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), __( '<span>%</span> Comments', 'bonestheme' ) );?>
 							</p>
-	     					<?php printf( '<p class="footer-category">' . __('filed under', 'bonestheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
+	     					<?php printf( '<p class="footer-category">' . __('', 'bonestheme' ) . '%1$s</p>' , get_the_category_list(', ') ); ?>
 	     					 <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
 						</footer>
 					</article>
