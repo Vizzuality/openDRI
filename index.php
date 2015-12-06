5cf2b274153ba5fbc3c9025304bb748feddb9f6b
Adrián Pérez <adrian@simbiotica.es>
2015-12-06 23:13:05 +0100

    resources card

--- index.php
+++ index.php
@@ -139,56 +139,13 @@
 					<span>view all resources</span>
 				</div>
 				<div class="row-container">
-					<?php 
-						$NEWS_COUNT = 1;
-						$news_i = 0;
-					?>
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<?php if ($news_i < $NEWS_COUNT) { ?>
-					<?php $news_i++; ?>
-					<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
-						<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-							$image = ($image[0]) ? $image[0] : get_template_directory_uri().'/library/images/red-cross.jpg';
-						?>
-            			<span class="img" style="background-image:url(<?php echo $image; ?>)"></span>
-						<header class="article-header">
-							<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-							<p class="byline entry-meta vcard">
-	                            <?php printf( __( '', 'bonestheme' ).' %1$s %2$s',
-	   								/* the time the post was published */
-	   								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-	   								/* the author of the post */
-	   								'<span class="by">'.__( 'by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-								); ?>
-							</p>
-						</header>
-						<section class="entry-content cf">
-							<?php the_content(); ?>
+					<article class="resource-cont">
+						<section>
+							<span>topic</span>
+							<hr>
+							<h3>training materials</h3>
 						</section>
-						<footer class="article-footer cf">
-							<p class="footer-comment-count">
-								<?php comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), __( '<span>%</span> Comments', 'bonestheme' ) );?>
-							</p>
-	     					<?php printf( '<p class="footer-category">' . __('', 'bonestheme' ) . '%1$s</p>' , get_the_category_list(', ') ); ?>
-	     					 <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-						</footer>
 					</article>
-					<? } ?>
-					<?php endwhile; ?>
-							<?php bones_page_navi(); ?>
-					<?php else : ?>
-							<article id="post-not-found" class="hentry cf">
-									<header class="article-header">
-										<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-								</header>
-									<section class="entry-content">
-										<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-								</section>
-								<footer class="article-footer">
-										<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
-								</footer>
-							</article>
-					<?php endif; ?>
 				</div>
 			</div>
 			<?php /* get_sidebar(); */ ?>
