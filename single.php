dfc3701fa091f84985ca4e460e5897621141d876
Adrián Pérez <adrian@simbiotica.es>
2015-12-08 19:47:41 +0100

    recent posts

--- single.php
+++ single.php
@@ -48,7 +48,38 @@
 							</article>
 
 						<?php endif; ?>
+						<h3>recent news</h3>
+						<ul>
+						<?php
+							$args = array( 'numberposts' => '3' );
+							$recent_posts = wp_get_recent_posts( $args );
+							foreach( $recent_posts as $recent ){ ?>
+								<article id="post-<?php echo $recent["ID"]; ?>" <?php post_class( 'cf' ); ?> role="article">
+									<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $recent["ID"] ), 'single-post-thumbnail' );
+										$image = ($image[0]) ? $image[0] : get_template_directory_uri().'/library/images/red-cross.jpg';
+									?>
+			            			<span class="img" style="background-image:url(<?php echo $image; ?>)"></span>
 
+									<header class="article-header">
+										<h1 class="h2 entry-title"><a href="<?php echo $recent["guid"]; ?>" rel="bookmark" title="<?php echo $recent["post_title"]; ?>"><?php echo $recent["post_title"]; ?></a></h1>
+									</header>
+									<section class="entry-content cf">
+										<?php echo $recent["post_content"]; ?>
+									</section>
+									<footer class="article-footer cf">
+										<p class="byline entry-meta vcard">
+				                            <?php printf( __( '', 'bonestheme' ).' %1$s %2$s',
+				   								/* the author of the post */
+				   								'<span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>',
+				   								/* the time the post was published */
+				   								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time('d M') . '</time>'
+											); ?>
+										</p>
+									</footer>
+								</article>
+							<?}
+						?>
+						</ul>
 					</main>
 
 					<?php /*get_sidebar(); */?>
