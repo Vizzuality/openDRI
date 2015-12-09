08f0d048841b88318b095a350cb1673362070a0c
Adrián Pérez <adrian@simbiotica.es>
2015-12-09 13:42:59 +0100

    add map for archive when its a project list

--- archive.php
+++ archive.php
@@ -1,27 +1,37 @@
 <?php get_header(); ?>
 
+<?
+
+// the_archive_title( '<h1 class="page-title">', '</h1>' );
+if ( is_category() ) {
+	$title = sprintf( __( '%s' ), 	   single_cat_title( '', false ) );
+} elseif ( is_tag() ) {
+	$title = sprintf( __( 'Tag: %s' ), single_tag_title( '', false ) );
+}
+
+if(is_category() && $title ==='projects') {
+	echo '<div id="map" class="cdbmap"></div>';
+} elseif (is_category() && $title === 'news') {
+	echo '<span class="corner-map"></span>';
+}
+
+?>
 			<div id="content">
 
 				<div id="inner-content" class="wrap cf">
 
-						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+						<main id="main" class="" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
 							<?php
-							// the_archive_title( '<h1 class="page-title">', '</h1>' );
-							if ( is_category() ) {
-								$title = sprintf( __( '%s' ), 	   single_cat_title( '', false ) );
-							} elseif ( is_tag() ) {
-								$title = sprintf( __( 'Tag: %s' ), single_tag_title( '', false ) );
-							}
 							echo '<h1 class="page-title">'.$title.'</h1>';
-							the_archive_description( '<div class="taxonomy-description">', '</div>' );
 							?>
-							<div id="list-content" class="m-all cf index-row" role="news" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+							<h3>Vestibulum id ligula porta felis euismod semper. Nullam id dolor. Ligula porta felis euismod semper ipsum. Vestibulum id ligula porta felis euismod semper. Nullam id dolor. Ligula porta felis euismod semper ipsum. Ullam id dolor.</h3>
+							<div id="list-content" class="m-all cf index-row" role="news">
 							<div class="row-container">
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
-								<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( the_ID() ), 'single-post-thumbnail' );
+								<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
 							$image = ($image[0]) ? $image[0] : get_template_directory_uri().'/library/images/red-cross.jpg';
 						?>
             					<span class="img" style="background-image:url(<?php echo $image; ?>)"></span>
@@ -30,7 +40,7 @@
 									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
 								</header>
 								<section class="entry-content cf">
-									<p><?php the_excerpt(); ?></p>
+									<?php the_excerpt(); ?>
 								</section>
 								<footer class="article-footer cf">
 									<p class="byline entry-meta vcard">
