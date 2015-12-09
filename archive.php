4b4783d4832d6f886a0f18bd21b1fdbb0c6283ef
Adrián Pérez <adrian@simbiotica.es>
2015-12-09 15:57:03 +0100

    code indentation

--- archive.php
+++ archive.php
@@ -27,35 +27,35 @@ if(is_category() && $title ==='projects') {
 							?>
 							<h3>Vestibulum id ligula porta felis euismod semper. Nullam id dolor. Ligula porta felis euismod semper ipsum. Vestibulum id ligula porta felis euismod semper. Nullam id dolor. Ligula porta felis euismod semper ipsum. Ullam id dolor.</h3>
 							<div id="list-content" class="m-all cf index-row" role="news">
-							<div class="row-container">
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+								<div class="row-container">
+								<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
-								<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-							$image = ($image[0]) ? $image[0] : get_template_directory_uri().'/library/images/red-cross.jpg';
-						?>
-            					<span class="img" style="background-image:url(<?php echo $image; ?>)"></span>
+								<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+									<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+								$image = ($image[0]) ? $image[0] : get_template_directory_uri().'/library/images/red-cross.jpg';
+									?>
+	            					<span class="img" style="background-image:url(<?php echo $image; ?>)"></span>
 
-								<header class="article-header">
-									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
-								</header>
-								<section class="entry-content cf">
-									<?php the_excerpt(); ?>
-								</section>
-								<footer class="article-footer cf">
-									<p class="byline entry-meta vcard">
-			                            <?php printf( __( '', 'bonestheme' ).' %1$s %2$s',
-			   								/* the author of the post */
-			   								'<span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>',
-			   								/* the time the post was published */
-			   								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time('d M') . '</time>'
-										); ?>
-									</p>
-								</footer>
-							</article>
+									<header class="article-header">
+										<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
+									</header>
+									<section class="entry-content cf">
+										<?php the_excerpt(); ?>
+									</section>
+									<footer class="article-footer cf">
+										<p class="byline entry-meta vcard">
+				                            <?php printf( __( '', 'bonestheme' ).' %1$s %2$s',
+				   								/* the author of the post */
+				   								'<span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>',
+				   								/* the time the post was published */
+				   								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time('d M') . '</time>'
+											); ?>
+										</p>
+									</footer>
+								</article>
 
-							<?php endwhile; ?>
-						</div>
+								<?php endwhile; ?>
+							</div>
 						</div>
 									<?php bones_page_navi(); ?>
 
