c88ee4acf5a8ab11b3cb8a26102b9ccbf036e5f8
Adrián Pérez <adrian@simbiotica.es>
2015-12-09 15:57:39 +0100

    search results cards

--- search.php
+++ search.php
@@ -4,46 +4,39 @@
 
 				<div id="inner-content" class="wrap cf">
 
-					<main id="main" class="m-all t-2of3 d-5of7 cf" role="main">
+					<main id="main" class="" role="main">
 						<h1 class="archive-title"><span><?php _e( 'Search Results for:', 'bonestheme' ); ?></span> <?php echo esc_attr(get_search_query()); ?></h1>
 
-						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+						<div id="list-content" class="m-all cf index-row" role="news">
+								<div class="row-container">
+								<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article">
+								<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+									<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+								$image = ($image[0]) ? $image[0] : get_template_directory_uri().'/library/images/red-cross.jpg';
+									?>
+	            					<span class="img" style="background-image:url(<?php echo $image; ?>)"></span>
 
-								<header class="entry-header article-header">
+									<header class="article-header">
+										<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
+									</header>
+									<section class="entry-content cf">
+										<?php the_excerpt(); ?>
+									</section>
+									<footer class="article-footer cf">
+										<p class="byline entry-meta vcard">
+				                            <?php printf( __( '', 'bonestheme' ).' %1$s %2$s',
+				   								/* the author of the post */
+				   								'<span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>',
+				   								/* the time the post was published */
+				   								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time('d M') . '</time>'
+											); ?>
+										</p>
+									</footer>
+								</article>
 
-									<h3 class="search-title entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-
-                  						<p class="byline entry-meta vcard">
-                    							<?php printf( __( 'Posted %1$s by %2$s', 'bonestheme' ),
-                   							    /* the time the post was published */
-                   							    '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-                      							    /* the author of the post */
-                       							    '<span class="by">by</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                    							); ?>
-                  						</p>
-
-								</header>
-
-								<section class="entry-content">
-										<?php the_excerpt( '<span class="read-more">' . __( 'Read more &raquo;', 'bonestheme' ) . '</span>' ); ?>
-
-								</section>
-
-								<footer class="article-footer">
-
-									<?php if(get_the_category_list(', ') != ''): ?>
-                  					<?php printf( __( 'Filed under: %1$s', 'bonestheme' ), get_the_category_list(', ') ); ?>
-                  					<?php endif; ?>
-
-                 					<?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-
-								</footer> <!-- end article footer -->
-
-							</article>
-
-						<?php endwhile; ?>
+								<?php endwhile; ?>
+							</div>
 
 								<?php bones_page_navi(); ?>
 
@@ -65,7 +58,7 @@
 
 						</main>
 
-							<?php get_sidebar(); ?>
+							<?php // get_sidebar(); ?>
 
 					</div>
 
