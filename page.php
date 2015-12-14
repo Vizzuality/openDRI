fd44f8201e4540a14f49f648d9dbd2c51c379d6e
Adrián Pérez <adrian@simbiotica.es>
2015-12-14 23:15:43 +0100

    about page content

--- page.php
+++ page.php
@@ -116,7 +116,7 @@ elseif ( is_page( 'about' ) ) {
 				<div id="inner-content" class="wrap cf">
 
 						<main id="main" class="m-all cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-							<div id="about" class="m-all cf index-row">
+							<div id="about" class="m-all cf index-row about-content">
 								<div class="card-third">
 									<h3>OpenDRI background</h3>
 									<p>In 2011, GFDRR launched the Open Data for Resilience Initiative (OpenDRI) to apply the concepts of the global open data movement to the challenges of reducing vulnerability to natural hazards and the impacts of climate change. OpenDRI supports World Bank Regional Disaster Risk Management Teams to build capacity and long-term ownership of open data projects with client countries that are tailored to meet specific needs and goals of stakeholders. OpenDRI engages with client governments in three main areas:</p>
@@ -136,6 +136,23 @@ elseif ( is_page( 'about' ) ) {
 								<p>
 									To engage communities in the creation of accurate and timely data about the rapidly evolving urban and rural environments in the place they live, OpenDRI works with governments and local communities to utilize simple, collaborative, crowdsourcing mapping tools such as OpenStreetMap (OSM). OpenDRI has also created and is supervising the Open Cities Project (http://www.opencitiesproject.org/) that facilitates community-mapping activities.
 								</p>
+							</div>
+							<div class="p-info">
+								<h4>risk cisualizaion and communication</h4>
+								<p>
+									To communicate risk more effectively to decision-makers in planning, preparedness and response activities, OpenDRI works with governments and partners to develop InaSAFE software. By combining data from scientists, local governments and communities, InaSAFE provides insights into the likely effects of disaster events.
+								</p>
+							</div>
+							<div class="p-info double">
+								<p>
+									The OpenDRI team, in partnership with international and national agencies, has developed this suite of complementary tools to improve risk information through better access to data. These tools have global developer and user communities, all of whom contribute to the ongoing use and development of the tools – all of which are aimed at providing better information for decision makers at all levels to take action to reduce, prepare for, and recover from disaster risks. While engaging with government to leverage the usage of these tools, the OpenDRI also strive to create local communities of user and developers involving government agencies, universities, NGOs, innovation hub to create sustainable capacity.
+								</p>
+							</div>
+							<div id="" class="m-all cf index-row about-content">
+								<div class="card-third">
+									<h3>Our principles</h3>
+									<p>In 2011, GFDRR launched the Open Data for Resilience Initiative (OpenDRI) to apply the concepts of the global open data movement to the challenges of reducing vulnerability to natural hazards and the impacts of climate change. OpenDRI supports World Bank Regional Disaster Risk Management Teams to build capacity and long-term ownership of open data projects with client countries that are tailored to meet specific needs and goals of stakeholders. OpenDRI engages with client governments in three main areas:</p>
+								</div>
 							</div>
 						</main>
 
