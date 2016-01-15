60ebe79e4a2c81358faee8b1d22d2172bb93c0dd
Adrián Pérez <adrian@simbiotica.es>
2016-01-15 17:22:56 +0100

    no results found markup on archive

--- archive.php
+++ archive.php
@@ -60,17 +60,26 @@ if(is_post_type_archive()) {
 
 							<?php else : ?>
 
-									<article id="post-not-found" class="hentry cf">
-										<header class="article-header">
-											<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-										<section class="entry-content">
-											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the archive.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
+									<div id="content">
+
+				<div id="inner-content" class="wrap cf">
+
+					<main id="main" class="m-all t-2of3 d-5of7 cf no-results" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+
+							<section class="search">
+
+									<p><?php get_search_form(); ?></p>
+
+							</section>
+							<section>
+								<a href="<?php echo home_url(); ?>" class="home-404">go to homepage</a>
+							</section>
+					</main>
+
+				</div>
+
+			</div>
 
 							<?php endif; ?>
 
