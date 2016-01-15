04013464238e560b8719317b720f2de658d9b7c2
Adrián Pérez <adrian@simbiotica.es>
2016-01-15 17:22:48 +0100

    no results found markup

--- search.php
+++ search.php
@@ -42,17 +42,25 @@
 
 							<?php else : ?>
 
-									<article id="post-not-found" class="hentry cf">
-										<header class="article-header">
-											<h1><?php _e( 'Sorry, No Results.', 'bonestheme' ); ?></h1>
-										</header>
-										<section class="entry-content">
-											<p><?php _e( 'Try your search again.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the search.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
+									<div id="content">
+
+				<div id="inner-content" class="wrap cf">
+
+					<main id="main" class="m-all t-2of3 d-5of7 cf no-results" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+							<section class="search">
+
+									<p><?php get_search_form(); ?></p>
+
+							</section>
+							<section>
+								<a href="<?php echo home_url(); ?>" class="home-404">go to homepage</a>
+							</section>
+					</main>
+
+				</div>
+
+			</div>
 
 							<?php endif; ?>
 
