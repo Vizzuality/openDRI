d94a01d2ad47dfdb84c6ce45d585b31bbf214929
Adrián Pérez <adrian@simbiotica.es>
2016-01-15 17:05:57 +0100

    custom searchform for 404

--- searchform.php
+++ searchform.php
@@ -1,7 +1,8 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
     <div>
         <label for="s" class="screen-reader-text"><?php _e('Search for:','bonestheme'); ?></label>
-        <input type="search" id="s" name="s" value="" />
+        <img src="<?php echo get_template_directory_uri(); ?>/library/images/search.svg">
+        <input type="search" id="s" name="s" value="" placeholder="Find what you're looking for"/>
 
         <button type="submit" id="searchsubmit" ><?php _e('Search','bonestheme'); ?></button>
     </div>
