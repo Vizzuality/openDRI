f62fa260ecb31a4d4a06da0406b4e1db506e6672
apercas <aloha@adrianperez.info>
2016-02-02 11:41:22 +0100

    remove POSTED BY

--- search.php
+++ search.php
@@ -25,9 +25,7 @@
 									</section>
 									<footer class="article-footer cf">
 										<p class="byline entry-meta vcard">
-				                            <?php printf( __( '', 'bonestheme' ).' %1$s %2$s',
-				   								/* the author of the post */
-				   								'<span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>',
+				                            <?php printf( __( '', 'bonestheme' ).' %1$s',
 				   								/* the time the post was published */
 				   								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time('d M') . '</time>'
 											); ?>
