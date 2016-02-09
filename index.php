6224137e5c47033470ddd8008a4ef85b29cd8ea9
adrian perez <adrianperez@berkley.local>
2016-02-09 10:44:48 +0100

    click on picture on index to go to item

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 						<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $recent["ID"] ), 'single-post-thumbnail' );
 							$image = ($image[0]) ? $image[0] : get_template_directory_uri().'/library/images/red-cross.jpg';
 						?>
-            			<span class="img" style="background-image:url(<?php echo $image; ?>)"></span>
+            			<a href="<?php echo $recent["guid"]; ?>" rel="bookmark" title="<?php echo $recent["post_title"]; ?>"><span class="img" style="background-image:url(<?php echo $image; ?>)"></span></a>
 
 						<header class="article-header">
 							<h1 class="h2 entry-title"><a href="<?php echo $recent["guid"]; ?>" rel="bookmark" title="<?php echo $recent["post_title"]; ?>"><?php echo $recent["post_title"]; ?></a></h1>
