f7adc4ae8d93b9401f33ee19e08800bf63e41c1d
Adrián <aloha@adrianperez.info>
2016-02-16 10:09:58 +0100

    display odri logo on admin

--- functions.php
+++ functions.php
@@ -222,6 +222,19 @@ function save_on_cartodb( $post_id ) {
   }
 }
 add_action( 'save_post', 'save_on_cartodb', 10, 3  );
+
+
+function my_login_logo() { ?>
+    <style type="text/css">
+        .login h1 a {
+            background-image: url(<?php echo get_template_directory_uri(); ?>/logo.svg);
+            padding-bottom: 30px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+
 /************* ACTIVE SIDEBARS ********************/
 
 // Sidebars & Widgetizes Areas
