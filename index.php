38c8c93b97c1c5ff8447df6c560ecd3a1caf03f1
Adrián <aloha@adrianperez.info>
2016-02-16 16:04:54 +0100

    date format fix YES. AGAIN.

--- index.php
+++ index.php
@@ -169,7 +169,7 @@
 						<li>
 							<a href="<?php echo $featured["guid"]; ?>">
 								<p><span class="title"><?php echo $featured["post_title"]; ?></span><span class="format"></span></p>
-								<p><span class="name"></span><span class="size"><? echo date_format(strtotime($featured["post_date_gmt"]),'Y-m-d') ?></span>
+								<p><span class="name"></span><span class="size"><? echo date('Y-m-d', strtotime($featured['post_date'])) ?></span>
 								</p>
 							</a>
 						</li>
