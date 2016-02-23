8fb4b43dfadec07116c7f2f66e9fa0dc35b32422
Adrián <aloha@adrianperez.info>
2016-02-23 22:10:01 +0100

    new copies

--- archive.php
+++ archive.php
@@ -93,7 +93,7 @@ if(is_post_type_archive() && $post_type=='project') {
 	echo '	<div class="blue-bar-top" id="blue-bar">
 				<div class="wrap wrapper filters">
 						<div id="blue-bar-pick-pillar">
-							<span class="title">Select:</span>
+							<span class="title">filter by pillar:</span>
 							<span class="option-pillar" data-option="open"><a href="'.home_url().'/category/pillars/open-data-platforms/" data-option="open"><i class="img-pile-1"></i>open data platforms</a></span>
 							<span class="option-pillar" data-option="community"><a href="'.home_url().'/category/pillars/community-mapping/" data-option="community"><i class="img-pile-2"></i>community mapping</a></span>
 							<span class="option-pillar" data-option="risk"><a href="'.home_url().'/category/pillars/risk-visualizations/" data-option="risk"><i class="img-pile-3"></i>risk visualization</a></span>
