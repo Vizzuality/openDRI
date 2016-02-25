9c092eca2edc2239363ed305242d41e36bb60b5f
Adrián <aloha@adrianperez.info>
2016-02-25 13:06:56 +0100

    project type tags

--- functions.php
+++ functions.php
@@ -246,13 +246,14 @@ add_filter('pre_get_posts', 'query_post_type');
 function query_post_type($query) {
   if(is_category() || is_tag()) {
     $post_type = get_query_var('post_type');
-  if($post_type)
-      $post_type = $post_type;
-  else
-      $post_type = array('post','project'); // replace cpt to your custom post type
-    $query->set('post_type',$post_type);
-  return $query;
+    if($post_type)
+        $post_type = $post_type;
+    else{
+        $post_type = array('post','project','resource');
     }
+    $query->set('post_type',$post_type);
+    return $query;
+  }
 }
 /************* ACTIVE SIDEBARS ********************/
 
