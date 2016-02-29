d6612f91a7b0ea9dc715d990e66564b05ca7e3ed
Adrián <aloha@adrianperez.info>
2016-02-29 17:52:24 +0100

    filter regions and news

--- archive.php
+++ archive.php
@@ -208,7 +208,9 @@ if(is_post_type_archive() && $post_type=='project') {
 												<span>
 													<?php
 													foreach((get_the_category()) as $category) { 
-														if ($category->parent == 2 || $category->parent == 5) break;
+														if ($category->parent == 2 || 
+															$category->parent == 5 ||
+															$category_>parent == 3) break;
 													    echo '<a href="'.esc_url( get_category_link( $category->term_id ) ).'">'.$category->cat_name . '</a> '; 
 													} 
 													?>
