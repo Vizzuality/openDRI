de2568f3c52bbf0b6c4e7084ac249febc9804983
Adrián <aloha@adrianperez.info>
2016-03-04 14:48:19 +0100

    country project number

--- footer.php
+++ footer.php
@@ -889,7 +889,8 @@
 						$('.amount-of-posts').hide();
 						var country = $(this).val().charAt(0).toUpperCase() + $(this).val().slice(1);
 						currentCountry = country;
-					  	$('#list-content').find("article[class*='"+ country +"']");
+					  	var $target = $('#list-content').find("article[class*='"+ country +"']");
+					  	$target.show();
 				  		$('#current-total-post-count').text($target.length);
 							sublayers[0].setSQL('SELECT * FROM wp_projects where LOWER(country_name) like \'%' + country.toLowerCase() + '%\' AND visible = true');
 							sublayers[1].setSQL("SELECT * FROM country_mask");
