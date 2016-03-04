68b4e289cbf6e2e9eb361504f54d8d1fbfdcb563
Adrián <aloha@adrianperez.info>
2016-03-04 11:05:37 +0100

    testing carto

--- functions.php
+++ functions.php
@@ -214,10 +214,17 @@ function save_on_cartodb( $post_id ) {
       // insert new row
       $query = "INSERT INTO wp_projects (wp_post_id, the_geom, name, location, url, pillar, region, iso, description, visible, country_name) VALUES ($post_id, ST_SetSRID(ST_Point($geodata__long, $geodata__lat),4326), '$geodata__title', '$geodata__address', '$geodata__url_title', '$pillar', '$region', '$geodata__country', '$geodata__content', '$visibility', '$geodata__c_name')"; 
     } else {
+      // check the post exists
+$query = "UPDATE wp_projects SET the_geom = ST_SetSRID(ST_Point($geodata__long, $geodata__lat),4326), name = '$geodata__title', location = '$geodata__address', url = '$geodata__url_title', pillar = '$pillar', region = '$region', iso = '$geodata__country', description = '$geodata__content', visible = '$visibility', country_name = '$geodata__c_name' WHERE wp_post_id = $post_id;
+INSERT INTO wp_projects (wp_post_id, the_geom, name, location, url, pillar, region, iso, description, visible, country_name)
+       SELECT $post_id, ST_SetSRID(ST_Point($geodata__long, $geodata__lat),4326), '$geodata__title', '$geodata__address', '$geodata__url_title', '$pillar', '$region', '$geodata__country', '$geodata__content', '$visibility', '$geodata__c_name'
+       WHERE NOT EXISTS (SELECT 1 FROM table WHERE id=$post_id)";
+
       // update existing row
-      $query = "UPDATE wp_projects SET the_geom = ST_SetSRID(ST_Point($geodata__long, $geodata__lat),4326), name = '$geodata__title', location = '$geodata__address', url = '$geodata__url_title', pillar = '$pillar', region = '$region', iso = '$geodata__country', description = '$geodata__content', visible = '$visibility', country_name = '$geodata__c_name' WHERE wp_post_id = $post_id "; 
+      //$query = "UPDATE wp_projects SET the_geom = ST_SetSRID(ST_Point($geodata__long, $geodata__lat),4326), name = '$geodata__title', location = '$geodata__address', url = '$geodata__url_title', pillar = '$pillar', region = '$region', iso = '$geodata__country', description = '$geodata__content', visible = '$visibility', country_name = '$geodata__c_name' WHERE wp_post_id = $post_id "; 
     }
     $url .= urlencode($query).$api_bit;
+    var_dump($url;)
     $response = wp_remote_get($url);
   }
 }
