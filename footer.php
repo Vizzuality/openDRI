4147c48245bdbdf6b8dfbf5b46bf85b202f0d52d
Adrián <aloha@adrianperez.info>
2016-04-05 18:17:49 +0200

    add OPENDRIs own newsletter

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
 							</div>
 							<div class="-ft-newsletter">
 								<span>Sign up for our newsletter:</span> 
-								<form action="//vizzuality.us10.list-manage.com/subscribe/post?u=5450ec66874ec1182c96d7ec7&amp;id=70766ac561" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+								<form action="//opendri.us13.list-manage.com/subscribe/post?u=2931499143c058886c415c5b9&amp;id=ca4856ec1b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 									<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="your.email@here">
 									<input type="text" name="b_5450ec66874ec1182c96d7ec7_70766ac561" tabindex="-1" value="" style="display:none">
 									<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
