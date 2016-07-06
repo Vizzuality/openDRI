fca6b3a6dc4a59accc43c045ca5e995249408146
Adrián <aloha@adrianperez.info>
2016-07-06 10:47:09 +0200

    display thumbnail pic on related news of projects

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,8 @@
 
-
 	<?php
 		// $meta = get_post_meta(get_the_ID(), 'resource', true);
 		$news = get_post_meta(get_the_ID(), 'news', true);
-		if (get_post_type() == 'project' && strlen($news)) {
+		if ((get_post_type() == 'project' || get_post_type() == 'news') && strlen($news)) {
 	?>		
 		<div id="sidebar1" class="sidebar m-all t-1of3 d-2of7 last-col cf" role="complementary">
 			<?php if (strlen($news) > 0) {
@@ -15,10 +14,10 @@
 					foreach(explode("\n", $news) as $line) {
 						$current_post = url_to_postid( $line );
 						$resource 	  = get_post($current_post);
-						$image = wp_get_attachment_image_src( get_post_thumbnail_id( $resource->ID ), 'single-post-thumbnail' );
+						$image = get_post_meta($current_post, 'thumbnailPic', true);
 						$fallbackimg = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ));
 						$image = ($image[0]) ? $image : $fallbackimg[0];
-						$image = ($image[0]) ? $image[0] : get_template_directory_uri().'/library/images/red-cross.jpg';
+						$image = ($image[0]) ? $image : get_template_directory_uri().'/library/images/red-cross.jpg';
 				?>
 				<li>
 					<p><a href="<?php echo $resource->guid; ?>" rel="bookmark" title="<?php echo $resource->post_title; ?>"><span class="img" style="background-image:url(<?php echo $image; ?>)"></span><i><?php echo $resource->post_title; ?></a></i></p></li>
