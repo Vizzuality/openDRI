29e12ba88b7600835eda898a3eba820bf1e8eba7
Adrián <aloha@adrianperez.info>
2016-07-07 17:57:31 +0200

    change onload for onready

--- footer.php
+++ footer.php
@@ -958,9 +958,7 @@
 					return false;
 				};
 			}
-			window.onload = function() {
-				init();
-			};
+			$(document).ready(init()); 
 
 			// Twitter
 			!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
