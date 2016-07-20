a39781e4c2a7110d5cec68363705962d712f9985
Adrián <aloha@adrianperez.info>
2016-07-21 00:06:03 +0200

    about styles

--- functions.php
+++ functions.php
@@ -368,7 +368,7 @@ can replace these fonts, change it in your scss files
 and be up and running in seconds.
 */
 function bones_fonts() {
-  wp_enqueue_style('googleFonts', 'http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
+  wp_enqueue_style('googleFonts', '//fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
 }
 
 add_action('wp_enqueue_scripts', 'bones_fonts');
