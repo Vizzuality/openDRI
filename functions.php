e259d09abdddf9337e9704f3a4c98a99a9c4219a
Adrián <aloha@adrianperez.info>
2016-08-05 18:19:52 +0200

    draft post

--- functions.php
+++ functions.php
@@ -232,7 +232,11 @@ function save_on_cartodb( $post_id ) {
     } else {
       $visibility = 'true';
     }
-
+    if ($_REQUEST["original_post_status"] == 'auto-draft' || $_REQUEST["original_post_status"] == 'draft') {
+      $api_bit = "&api_key=$cdb_api_key";
+      wp_remote_get("https://opendri.cartodb.com/api/v2/sql?q=".urlencode("UPDATE wp_projects SET visible = false WHERE wp_post_id = ".$post_id.";").$api_bit);
+      return;
+    }
     if ($_REQUEST["original_publish"] != 'Update') {
       // insert new row
       $query = "INSERT INTO wp_projects (wp_post_id, the_geom, name, location, url, pillar, region, iso, description, visible, country_name) VALUES ($post_id, ST_SetSRID(ST_Point($geodata__long, $geodata__lat),4326), '$geodata__title', '$geodata__address', '$geodata__url_title', '$pillar', '$region', '$geodata__country', '$geodata__content', '$visibility', '$geodata__c_name')"; 
